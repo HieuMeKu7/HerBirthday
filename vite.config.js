d64206ec14492a0969a3c5ae015a32f771f9f19f
Dương Trung Hiếu <105829349+HieuMeKu7@users.noreply.github.com>
2025-05-09 21:49:34 +0000

    fgg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
  
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['eb2faff4-81f5-46b6-8c32-8abaded10d70-00-2t92htbfwfv9f.sisko.replit.dev']
+  }
 }); 
